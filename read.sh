ebc23db7e20d79a19745801af8387a19038d74a1
shashidhar <raoshashidhar.3@gmail.com>
2021-02-10 18:03:30 -0500

    data variable

--- read.sh
+++ read.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "enter the value of A" $A
-read -p "Enter the value of B" $B
-SUM=$((A + B))
+read -p "enter the value of A" $num1
+read -p "Enter the value of B" $num2
+SUM=$(($num1 + $num2))
 echo -e "value of A+B= $SUM"
