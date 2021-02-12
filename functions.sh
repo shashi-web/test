a386124d468570b95195d27c61e97c4b1dcabb9b
shashidhar <raoshashidhar.3@gmail.com>
2021-02-12 00:35:28 -0500

    adding functions command

--- functions.sh
+++ functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sample() {
-  read -p "enter the value of A=" ${num1}
-  read -p "enter the value of B=" ${num2}
+  read -r "enter the value of A=" ${num1}
+  read -r "enter the value of B=" ${num2}
   if [ "${num1}" == "${num2}" ]; then
     echo "$* "
     else
