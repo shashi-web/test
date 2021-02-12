7e3a8ca4c81eef11da7d8150405a3c6505a170f7
shashidhar <raoshashidhar.3@gmail.com>
2021-02-12 02:14:44 -0500

    adding functions command

--- functions.sh
+++ functions.sh
@@ -2,7 +2,7 @@
 sample() {
   read -p "enter the value of A=" ${num1}
   read -p "enter the value of B=" ${num2}
-  if [ "${num1}" -ne "${num2}" ]; then
+  if [ "$num1" -ne "$num2" ]; then
     echo "$* "
     else
       echo "there are not equal"
