376d670076cfae32e15a592c5f6ce0a708ed2f66
shashidhar <raoshashidhar.3@gmail.com>
2021-02-12 00:34:14 -0500

    adding functions command

--- functions.sh
+++ functions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sample() {
-  read -p "enter the value of A=" $num1
-  read -p "enter the value of B=" $num2
-  if [ "$num1" == "$num2" ]; then
+  read -p "enter the value of A=" ${num1}
+  read -p "enter the value of B=" ${num2}
+  if [ "${num1}" == "${num2}" ]; then
     echo "$* "
     else
-      echo"there are not equal"
+      echo "there are not equal"
       fi
 }
 sample Both the values are equal
